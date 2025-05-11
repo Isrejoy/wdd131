d557dae83a2e42415d784e8409df6ee89d3c414f
Isrejoy <zhoushengjian1@gmail.com>
2025-05-10 18:27:40 -0600

    upload coolpics

---
+++ coolpics/coolpics.js
@@ -0,0 +1,8 @@
+// function toggleMenu() {
+//     const nav = document.getElementById('navigation');
+//     if (nav.style.display === 'block' && screen.width >= 700) {
+//         nav.style.display = 'none';
+//     } else {
+//         nav.style.display = 'block';
+//     }
+// }
